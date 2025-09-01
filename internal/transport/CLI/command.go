0b5c9de38ad0b1da8cb1193ebb890f0589de9fd2
Onni100 <user_ams@mypc.localdomain>
2025-09-01 23:08:49 +0300

    Осталось дописать реализацию help, перенёс вывод в отдельный функции

--- internal/transport/CLI/command.go
+++ internal/transport/CLI/command.go
@@ -1,5 +1,12 @@
 package CLI
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
 type Command struct {
 	args []string
 }
@@ -15,6 +22,23 @@ func (c *Command) isAdd() bool {
 	return true
 }
 
+func (c *Command) Add(h *Handler) {
+	ID, err := h.service.CreateHabit(os.Args[2])
+	if err != nil {
+		log.Fatal(fmt.Errorf("create habit: [%w]", err))
+	}
+	fmt.Printf("Была записана новая привычка: %s c айди %d\n", os.Args[2], ID)
+	habit, err := h.service.GetHabit(ID)
+	if err != nil {
+		log.Fatal(fmt.Errorf("get habit in create process: [%w]", err))
+	}
+	fmt.Printf("Нынешняя запись:\n")
+	fmt.Printf("Идентификатор: %d\n", habit.HabitID)
+	fmt.Printf("Название: %s\n", habit.Name)
+	fmt.Printf("Количество повторений: %d\n", habit.Repetitions)
+	fmt.Printf("Последнее повторение: %s\n", habit.LastRepetition)
+}
+
 func (c *Command) isList() bool {
 	if len(c.args) != 2 || c.args[1] != "list" {
 		return false
@@ -22,11 +46,39 @@ func (c *Command) isList() bool {
 	return true
 }
 
-func (c *Command) isGetHabitByID() bool {
+func (c *Command) List(h *Handler) {
+	hs, err := h.service.GetHabits()
+	if err != nil {
+		log.Fatal(fmt.Errorf("get habbits: [%w]", err))
+	}
+	for _, habit := range hs {
+		fmt.Printf("Идентификатор: %d\n", habit.HabitID)
+		fmt.Printf("Название: %s\n", habit.Name)
+		fmt.Printf("Количество повторений: %d\n", habit.Repetitions)
+		fmt.Printf("Последнее повторение: %s\n\n", habit.LastRepetition)
+	}
+}
+
+func (c *Command) isGetHabit() bool {
 	if len(c.args) != 3 || c.args[1] != "id" || c.args[2] == "" {
 		return false
 	}
 	return true
+}
+
+func (c *Command) GetHabit(h *Handler) {
+	id, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(fmt.Errorf("strconv id: [%w]", err))
+	}
+	habit, err := h.service.GetHabit(int64(id))
+	if err != nil {
+		log.Fatal(fmt.Errorf("get habit in self-process: [%w]", err))
+	}
+	fmt.Printf("Идентификатор: %d\n", habit.HabitID)
+	fmt.Printf("Название: %s\n", habit.Name)
+	fmt.Printf("Количество повторений: %d\n", habit.Repetitions)
+	fmt.Printf("Последнее повторение: %s\n\n", habit.LastRepetition)
 }
 
 func (c *Command) isDone() bool {
@@ -36,9 +88,53 @@ func (c *Command) isDone() bool {
 	return true
 }
 
+func (c *Command) Done(h *Handler) {
+	id, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(fmt.Errorf("strconv id: [%w]", err))
+	}
+	err = h.service.MarkHabitDone(int64(id))
+	if err != nil {
+		log.Fatal(fmt.Errorf("habit done: [%w]", err))
+	}
+}
+
 func (c *Command) isDelete() bool {
 	if len(c.args) != 3 || c.args[1] != "delete" || c.args[2] == "" {
 		return false
 	}
 	return true
+}
+
+func (c *Command) Delete(h *Handler) {
+	id, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(fmt.Errorf("strconv id: [%w]", err))
+	}
+
+	habit, err := h.service.GetHabit(int64(id))
+	if err != nil {
+		log.Fatal(fmt.Errorf("get habit in delete: [%w]", err))
+	}
+
+	err = h.service.DeleteHabit(int64(id))
+	if err != nil {
+		log.Fatal(fmt.Errorf("delete habit: [%w]", err))
+	}
+	fmt.Printf("Привычка снизу была удалена!\n")
+	fmt.Printf("Идентификатор: %d\n", habit.HabitID)
+	fmt.Printf("Название: %s\n", habit.Name)
+	fmt.Printf("Количество повторений: %d\n", habit.Repetitions)
+	fmt.Printf("Последнее повторение: %s\n\n", habit.LastRepetition)
+}
+
+func (c *Command) isHelp() bool {
+	if len(os.Args) != 2 || os.Args[1] != "help" {
+		return false
+	}
+	return true
+}
+
+func (c *Command) Help() {
+	
 }
